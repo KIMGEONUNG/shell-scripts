b39b5589d72375de1a2ca0d46d4bd610663424c5
comar <saywooong@gmail.com>
2020-04-28 09:27:26 +0900

    refac: remove redundant vote

--- diff.sh
+++ diff.sh
@@ -2,13 +2,12 @@
 
 echo "shcli code defference test"
 
-#old_path="C:\\Users\\comar\\SHIntegration_new\\SHCoreConsole"
-#new_path="C:\\Users\\comar\\SHIntegration_old\\SHServers\\SHCoreConsole"
+old_path=C:\\Users\\comar\\SHIntegration_new\\IPAnalyzer\\Kernel\\ReportMaker.cs
+new_path=C:\\Users\\comar\\SHIntegration_old\\SHServers\\IPAnalyzer\\Kernel\\ReportMaker.cs
 
-old_path="C:\\Users\\comar\\SHIntegration_new\\IPAnalyzer\\Kernel\\ReportMaker.cs"
-new_path="C:\\Users\\comar\\SHIntegration_old\\SHServers\\IPAnalyzer\\Kernel\\ReportMaker.cs"
+echo "old path : $old_path"
+echo "new path : $new_path"
 
-echo "old path : ${old_path}"
-echo "new path : ${new_path}"
+echo "diff start"
 
-diff -p ${old_path} ${new_path}
+diff -p $old_path $new_path
